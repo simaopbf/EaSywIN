18f9cc139c172029b9df97c9aac34e9bf438da74
João Amorim <up202009074@edu.fe.up.pt>
2023-12-05 19:15:47 +0000

    obrigadodeus

---
+++ process-signup.php
@@ -0,0 +1,14 @@
+<?php
+
+if ($_POST["password"] !== $_POST["password_confirmation"]) {
+    $error_message="Passwords must match";
+    include('index.php');
+    exit();
+}
+
+$password_hash = password_hash($S_POST["password"], PASSWORD_DEFAULT);
+
+print_r($_POST);
+var_dump($password_hash);
+
+?>

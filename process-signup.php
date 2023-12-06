2599ebd8f5fe97a01c7fb6f4199ce26b1be9a900
João Amorim <up202009074@edu.fe.up.pt>
2023-12-06 15:37:19 +0000

    ya

--- process-signup.php
+++ process-signup.php
@@ -8,6 +8,8 @@ if ($_POST["password"] !== $_POST["password_confirmation"]) {
 
 $password_hash = password_hash($S_POST["password"], PASSWORD_DEFAULT);
 
+$mysqli = require __DIR__ . "/database.php";
+
 print_r($_POST);
 var_dump($password_hash);
 

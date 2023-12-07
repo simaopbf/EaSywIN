027a8bccdbf55e9e20ef35edfa7d7ce273579747
franciscosacouto <francisco.sa.couto42@gmail.com>
2023-12-07 02:01:29 +0000

    tentei por responsive para o telemovel meio que da meio q nao da

--- homepage.php
+++ homepage.php
@@ -4,7 +4,7 @@
 </head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE-edge">
-<meta name="viewport" content="widt=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>EasyWin Home</title>
 <link rel="stylesheet" href="home.css">
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -23,6 +23,7 @@
             </ul>
         </div>
     </nav>
+    
     <div class="image_top">
         <img src="pexels-patrik-felker-6220559.jpg">
     </div>
@@ -65,6 +66,10 @@
     </div>
     <div class="heighttotal"></div>
 
+
+    
+
+    
     
 </body>
 

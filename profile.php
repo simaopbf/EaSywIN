5b6a09d4f4425587f77d599e377642d48ddf858e
Simao Francisco <up201907198@up.pt>
2023-12-10 01:04:18 +0000

    Profile page

--- profile.php
+++ profile.php
@@ -5,8 +5,8 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE-edge">
 <meta name="viewport" content="widt=device-width, initial-scale=1.0">
-<title>EasyWin Home</title>
-<link rel="stylesheet" href="home.css">
+<title>Profile</title>
+<link rel="stylesheet" href="profile.css">
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 <head>
 <body>
@@ -22,12 +22,61 @@
     
             </ul>
         </div>
-    </nav>
-    <div class="image_top">
+    </nav> 
+    <!--<div class="image_top">
         <img src="pexels-patrik-felker-6220559.jpg">
+    </div> -->
+    <div class = "header_wrapper">
+        <header></header>
+        <div class="cols_container">
+            <div class ="left_col">
+                <div class ="img_container">
+                    <img src="camera.jpg" alt="User">
+                    <span></span>
+                </div>
+                <h2>Username</h2>
+                <p>E-mail</p>
+                <p>Telephone</p>
+
+                <ul class="about">
+                    <li><span>25</span>Friends</li>
+                    <li><span>2</span>Homes</li>
+                </ul>
+
+                <div class="content">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor maxime corporis eum natus, assumenda deleniti fugit nisi atque aut adipisci, animi, distinctio voluptas ducimus eligendi. Molestias sunt quo aliquam porro.
+                    </p>
+                 
+                    <ul>
+                        <li><i class="fab fa-twitter"></i></li>
+                        <li><i class="fab fa-pinterest"></i></li>
+                        <li><i class="fab fa-facebook"></i></li>
+                        <li><i class="fab fa-instagram"></i></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="right_col">
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Friends</a></li>
+                        <li><a href="#">Reservations</a></li>
+                    </ul>
+                    <button>Follow</button>
+
+                    <div class="Home">
+                        <img src="pexels-patrik-felker-6220559.jpg" alt="Photo">
+                        <img src="pexels-patrik-felker-6220559.jpg" alt="Photo">
+                        <img src="pexels-patrik-felker-6220559.jpg" alt="Photo">
+                        <img src="pexels-patrik-felker-6220559.jpg" alt="Photo">
+                        <img src="pexels-patrik-felker-6220559.jpg" alt="Photo">
+                    </div>
+                </nav>
+            </div>
+        </div>
     </div>
-
-
 </body>
 
 </html>

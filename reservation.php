ff07d3ebc210b18741267333655b5aa4a94ba3d0
franciscosacouto <francisco.sa.couto42@gmail.com>
2023-12-16 20:13:44 +0000

    pagina reserve

--- reservation.php
+++ reservation.php
@@ -1,12 +1,31 @@
+<?php
+  session_start();
+
+  $msg = $_SESSION['msg'];
+  unset($_SESSION['msg']);
+
+
+try {
+    $dbh = new PDO('sqlite:sql/database.db');
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    
+  } catch (PDOException $e) {
+    $error_msg = $e->getMessage();
+  }
+?>
+
+
 <!DOCTYPE html>
 <html lang="eng">
 
 </head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE-edge">
-<meta name="viewport" content="widt=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>EasyWin Home</title>
-<link rel="stylesheet" href="home.css">
+<link rel="stylesheet" href="styleReservations.css">
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 <head>
 <body>
@@ -16,17 +35,42 @@
                 <a href="homepage.php">EasyWIN</a>
             </div>
             <ul>
-                <li><a href="#">Maps</a> </li>
-                <li><a href="#howitworks">How it works</a> </li>    
-                <li><a class="backgroundcolor" href="login.php">Login</a> </li>
+            <?php if (!isset($_SESSION['username'])) { ?>   
+                    <li><a class="backgroundcolor" href="login.php"> <i class='bx bxs-user'></i>  Login</a> </li>
+                <?php } else { ?>
+                        <li><a class="backgroundcolor" href="profile.php"><i class='bx bxs-user'></i>  Profile </a>  </li>
+                <?php } ?>
+                
     
             </ul>
         </div>
     </nav>
-    <div class="image_top">
-        <img src="pexels-patrik-felker-6220559.jpg">
-    </div>
+    
 
+    </div>
+    <div class="center">
+        <div class="wrapper">
+             <h1>Make Reservations</h1>
+             <div class= "divide">
+
+                <div class= "house_sec_1">
+                   <h3>House (friend)</h3>
+                  <img src="airbnb alugar.png">
+                </div>
+                <div class= "house_sec_2">
+                    <h4>City: (city)</h4>
+                    <h4>Weather: fjdks</h4>
+                    <h4>Capacity: capacity</h4>
+                    <button>Reserve</button>
+
+
+            
+                
+                </div>
+            </div>
+           
+        </div>
+    </div>                
 </body>
 
 </html>

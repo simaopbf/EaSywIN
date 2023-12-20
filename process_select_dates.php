7ce9d2d01d501e9aab2ac49e32b562d5078d363e
franciscosacouto <francisco.sa.couto42@gmail.com>
2023-12-20 12:20:36 +0000

    debugging

--- process_select_dates.php
+++ process_select_dates.php
@@ -7,12 +7,22 @@ $username= $_SESSION['username'];
 $transport_type=$_POST['transport_type'];
 $n_guests=$_POST['n_guests'];
 $id= $_POST['id'];
-
+$city_departure= 'OPorto';
 function insertReservation($check_in, $check_out, $username, $transport_type, $n_guests,$capacity)
 {
   global $dbh;
   $stmt = $dbh->prepare('INSERT INTO Reservation (date_in,date_out,transportation_type,guest,number_of_guests, capacity) VALUES (?, ?, ?, ?, ?, ?)');
   $stmt->execute(array($check_in, $check_out, $transport_type, $username, $n_guests,$capacity));
+}
+
+$duration= strtotime($check_out)-strtotime($check_in);
+
+function insertBudget($total, $duration, $distance)
+{
+
+  global $dbh;
+  $stmt = $dbh->prepare('INSERT INTO Budget (total,duration,distance) VALUES (?, ?, ?)');
+  $stmt->execute(array($total, $duration, $distance));
 }
 
 
@@ -25,6 +35,19 @@ try {
     $stmt->bindValue(':id', $id, PDO::PARAM_INT);
     $stmt->execute();
     $accom = $stmt->fetchAll();
+    $stmt = $dbh->prepare('SELECT * FROM Transportation_type WHERE transportation_name=:id');
+    $stmt->bindValue(':id', $transport_type, PDO::PARAM_INT);
+    $stmt->execute();
+    $transportation_type = $stmt->fetchAll();
+    $stmt = $dbh->prepare('SELECT * FROM City WHERE name=:id');
+    $stmt->bindValue(':id', $city_departure, PDO::PARAM_INT);
+    $stmt->execute();
+    $city_departure_array= $stmt->fetchAll();
+    
+    $t_value=$transportation_type[0]['cost_per_km'];
+    $distance = acos((sin(deg2rad($city_departure_array[0]['lat']))* sin(deg2rad($accom[0]['lat']))) + (cos(deg2rad($city_departure_array[0]['lat'])) * cos(deg2rad($accom[0]['lat']))) * (cos(deg2rad($accom[0]['lon'])-deg2rad($city_departure_array[0]['lon'])))) * 6371;
+    $total= $t_value*$distance + $duration * $accom[0]['average_cost_of_living'];
+    insertBudget($total, $duration, $distance);
     if( strtotime($check_in)>=strtotime($accom[0]['date_on']) && strtotime($check_out)<=strtotime($accom[0]['date_off']) &&  $n_guests<= $accom[0]['capacity'] && strtotime($check_in)<=strtotime($check_out)){
         insertReservation($check_in, $check_out, $username, $transport_type, $n_guests, $accom[0]['capacity']);
         include('homepage.php');
